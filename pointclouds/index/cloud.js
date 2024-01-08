bbf8e02966ba96cc5fbe9966eb0163434067b787
user <pe_gasa@hotmail.com>
2024-01-08 15:20:01 -0600

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 63157205,
+    "boundingBox": {
+        "lx": 487984.125,
+        "ly": 2158538.0,
+        "lz": 2232.519287109375,
+        "ux": 488181.625,
+        "uy": 2158735.5,
+        "uz": 2430.019287109375
+    },
+    "tightBoundingBox": {
+        "lx": 487984.109775,
+        "ly": 2158538.0654,
+        "lz": 2232.5193060546876,
+        "ux": 488119.707775,
+        "uy": 2158735.534,
+        "uz": 2276.0781060546874
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 1.7104002237319947,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
